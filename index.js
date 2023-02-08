19bbecbd00da8f33fefe4f728734118d9bda34bb
Dominic Ferenczy <ferenczyd@Dominics-Mini.local>
2023-02-07 16:02:56 -0800

    Added custom JavaScript using event listeners.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i =0; i<numberOfDrumButtons; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click",function() {
-        alert("I got clicked");
-    });
+for (var i = 0; i < numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+    this.style.color="white"
+
+  });
 }
 
-
-
+//var audio = new Audio("sounds/tom-1.mp3");
+//audio.play();
